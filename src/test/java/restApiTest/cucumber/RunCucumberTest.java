16cfdaf2a908722112e38f3b46d0d069c9184247
Sunpreet Singh Rathor <rathorsunpreet@gmail.com>
2024-11-27 13:41:15 -0500

    Fixed Cucumber Errors

--- src/test/java/restApiTest/cucumber/RunCucumberTest.java
+++ src/test/java/restApiTest/cucumber/RunCucumberTest.java
@@ -1,5 +1,6 @@
 package restApiTest.cucumber;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
@@ -9,7 +10,9 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("restApiTest/cucumber")
+//@SelectClasspathResource("restApiTest/cucumber")
+@SelectClasspathResource("restApiTest")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "restApiTest")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, json:target/jsonReports/cucumber.json")
 public class RunCucumberTest {
 }
